934e09577b9fb4dc8f231803dd7c09608758c72e
Denis <dalekseenko8@gmail.com>
2025-11-12 14:59:26 +0300

    project add title

--- miniapp/src/components/mainScreens/ProjectsScreen.jsx
+++ miniapp/src/components/mainScreens/ProjectsScreen.jsx
@@ -1,6 +1,6 @@
 const ProjectsScreen = () => (
   <section className="screen">
-    <h2 className="screen__title">Projects</h2>
+    <h2 className="screen__title">Проектная деятельность</h2>
     <p className="screen__subtitle">Здесь будут проекты студентов и кружков.</p>
   </section>
 );
